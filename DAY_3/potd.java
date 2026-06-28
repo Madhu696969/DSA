23bda52ed730ab2744450209af8401a7cb183aed
Madhu696969 <2400030376@kluniversity.in>
2026-06-28 19:22:05 +0530

    day3 of 90

--- DAY_3/potd.java
+++ DAY_3/potd.java
@@ -1,2 +1,29 @@
 //3020. Find the Maximum Number of Elements in Subset
 
+class Solution {
+    public int maximumLength(int[] nums) {
+        int res=0;
+        HashMap<Long,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            long k=nums[i];
+            mp.put(k,mp.getOrDefault(k,0)+1);
+        }
+        for(long st:mp.keySet()){
+            long key=st;
+            int tt=0;
+            if(key==1){
+                int freq=mp.getOrDefault(key,0);
+                tt=freq%2==1?freq:freq-1;
+            }
+            else{
+                while(mp.getOrDefault(key,0)>=2 && mp.containsKey(key*key)){
+                    tt+=2;
+                    key=key*key;
+                }
+                tt+=1;
+            }
+            res=Math.max(res,tt);
+        }
+        return res;
+    }
+}
