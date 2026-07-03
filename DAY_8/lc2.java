5a9a6bc992b95ddb719c3f57cf5f7771de1e0af8
Madhu696969 <2400030376@kluniversity.in>
2026-07-03 21:24:46 +0530

    day8 of 90

--- DAY_8/lc2.java
+++ DAY_8/lc2.java
@@ -1,2 +1,31 @@
 // 1870. Minimum Speed to Arrive on Time
 
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int st=1,r=10000000;
+        int res=-1;
+        while(st<=r){
+            int mid=st+(r-st)/2;
+
+            if(Possible(dist,mid)<=hour){
+                res=mid;
+                r=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        return res;
+    }
+    private double Possible(int[] dist,int mid){
+        double time=0;
+        int n=dist.length;
+        for(int i=0;i<n-1;i++){
+            double t=(double)dist[i]/(double)mid;
+
+            time+=Math.ceil(t);
+        }
+        time+=(double)dist[n-1]/(double)mid;
+        return time;
+    }
+}
