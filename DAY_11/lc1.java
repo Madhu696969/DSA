5a78b8f01cc7c759c92b62f1148c90a54a342862
Madhu696969 <2400030376@kluniversity.in>
2026-07-06 23:14:54 +0530

    day11 of 90

--- DAY_11/lc1.java
+++ DAY_11/lc1.java
@@ -1,2 +1,42 @@
 // 2968. Apply Operations to Maximize Frequency Score
 
+class Solution {
+    public int maxFrequencyScore(int[] nums, long k) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        long[] pref=new long[n];
+        pref[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pref[i]=pref[i-1]+nums[i];
+        }
+        int min=1,max=n;
+        int res=0;
+        while(min<=max){
+            int mid=min+(max-min)/2;
+            if(isPossible(mid,nums,k,pref)){
+                res=mid;
+                min=mid+1;
+            }
+            else{
+                max=mid-1;
+            }
+        }
+        return res;
+    }
+    private boolean isPossible(int len,int[] nums,long k,long[] pref){
+        int i=0,j=len-1;
+        while(j<nums.length){
+            int mid=(j+i)/2;
+            long tar=nums[mid];
+            long leftSum = (mid >0?pref[mid - 1] : 0) - (i > 0 ? pref[i - 1] : 0);
+            long rightSum = pref[j] - pref[mid];
+            long leftCost = (long)(mid-i)*tar-leftSum;
+            long rightCost = rightSum -(long)(j - mid)*tar;
+            if (leftCost+rightCost<=k)
+            return true;
+            i++;
+            j++;
+        }
+        return false;
+    }
+}
--- DAY_11/potd.java
+++ DAY_11/potd.java
@@ -1,5 +1,19 @@
 // 1288. Remove Covered Intervals
 
+// Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+
+// The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+
+// Return the number of remaining intervals.
+
+ 
+
+// Example 1:
+
+// Input: intervals = [[1,4],[3,6],[2,8]]
+// Output: 2
+// Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->{
