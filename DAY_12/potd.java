f544d44f4746e3ca4a8aaee3245ca8828b15bafd
Madhu696969 <2400030376@kluniversity.in>
2026-07-07 22:49:22 +0530

    day12 of 90

--- DAY_12/potd.java
+++ DAY_12/potd.java
@@ -1,2 +1,15 @@
 // 3754. Concatenate Non-Zero Digits and Multiply by Sum I
 
+class Solution {
+    public long sumAndMultiply(int n) {
+        if(n==0) return 0;
+        String p = String.valueOf(n).replaceAll("0", ""); 
+        int sum=0;
+        for(int i=0;i<p.length();i++){
+            int t=p.charAt(i)-'0';
+            sum+=t;
+        }
+        long r=Long.parseLong(p);
+        return (r*sum);
+    }
+}
