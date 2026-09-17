70f8a873182e4c20d949b29e3fcdb2808f301e3b
KaleruMadhu <2400030376@kluniversity.in>
2026-09-17 23:21:26 +0530

    Time: 6 ms (99.1%), Space: 90.8 MB (88.34%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -3,10 +3,7 @@ class Solution {
         int n=arr.length;
         int[] preMin=new int[n];
         Arrays.fill(preMin,Integer.MAX_VALUE);
-        int res=Integer.MAX_VALUE;
-        int curS=0;
-        int i=0,j=0;
-        int pM=Integer.MAX_VALUE;
+        int i=0,j=0,res=Integer.MAX_VALUE,prevMin=Integer.MAX_VALUE,curS=0;
         while(j<n){
             curS+=arr[j];
             while(curS>target){
@@ -17,9 +14,9 @@ class Solution {
                 if(i>0 && preMin[i-1]!=Integer.MAX_VALUE){
                     res=Math.min(res,len+preMin[i-1]);
                 }
-                pM=Math.min(len,pM);
+                prevMin=Math.min(len,prevMin);
             }
-            preMin[j]=pM;
+            preMin[j]=prevMin;
             j++;
         }
         return res==Integer.MAX_VALUE?-1:res;
